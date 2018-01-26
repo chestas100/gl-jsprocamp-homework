1a1de274ca796cc482c0e869281f7b3c474f8a95
Stas <chstas2@gmail.com>
2018-01-27 00:01:04 +0200

    add raw lesson2

--- src/lesson2/task.js
+++ src/lesson2/task.js
@@ -1,29 +1,31 @@
 /*
   Напишите функцию, которая параметрами принимает 2 числа и возвращает их сумму
 */
-export function sum(a, b) {
-
+export function sum(a = 0, b = 0) {
+  return a + b;
 }
 
 /*
   Напишите функцию, которая возвращает сумму всех чисел, что передаются параметрами
 */
-export function sumAll() {
-
+export function sumAll(...args) {
+  return args.reduce((sum, el) => {
+    return  sum + el;
+  }, 0)
 }
 
 /*
   Напишите функцию, которая возвращает число x в степень n
 */
 export function pow(x, n) {
-
+  return x**n
 }
 
 /*
   Напишите функцию, которая возвращает рандомное целое число от from до to
 */
 export function random(from, to) {
-
+  return Math.random() * (to - from) + from;
 }
 
 export default {
