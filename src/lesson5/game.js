7bb2de94c269aa9e39af57f601471f54f7fe7e3c
chestas100 <chestas100@gmail.com>
2018-02-19 23:02:43 +0200

    lesson5 fix game.js

--- src/lesson5/game.js
+++ src/lesson5/game.js
@@ -6,141 +6,133 @@ function getRandomInt(min, max) {
 
 const GameCharacter = function GameCharacter(charClass) {
   this.charClass = charClass.charAt(0).toUpperCase() + charClass.slice(1).toLowerCase();
-
-  this.getCharInitData = function getCharInitData(type) {
-    const data = {
-      warrior: {
-        life: 30,
-        damage: 4,
-      },
-      rogue: {
-        life: 25,
-        damage: 3,
-      },
-      sorcerer: {
-        life: 20,
-        damage: 5,
-      },
-      zombie: {
-        life: 8,
-        damage: 4,
-      },
-      skeleton: {
-        life: 10,
-        damage: 6,
-      },
-      holem: {
-        life: 15,
-        damage: 6,
-      },
-    };
-    const charToSend = data[this.charClass.toLowerCase()];
-    if (charToSend) {
-      return charToSend[type];
-    }
-    return 'Incorrect character class provided';
-  };
-
   this.life = this.getCharInitData('life');
   this.damage = this.getCharInitData('damage');
+};
 
-  this.getName = function getName() {
-    if (this.name) {
-      return this.name;
+GameCharacter.prototype.getCharInitData = function getCharInitData(type) {
+  const data = {
+    warrior: {
+      life: 30,
+      damage: 4,
+    },
+    rogue: {
+      life: 25,
+      damage: 3,
+    },
+    sorcerer: {
+      life: 20,
+      damage: 5,
+    },
+    zombie: {
+      life: 8,
+      damage: 4,
+    },
+    skeleton: {
+      life: 10,
+      damage: 6,
+    },
+    holem: {
+      life: 15,
+      damage: 6,
+    },
+  };
+  const charToSend = data[this.charClass.toLowerCase()];
+  if (charToSend) {
+    return charToSend[type];
+  }
+  return 'Incorrect character class provided';
+};
+
+GameCharacter.prototype.getCharClass = function getCharClass() {
+  return this.charClass;
+};
+
+GameCharacter.prototype.attack = function attack(target) {
+  const attackerRole = this instanceof Hero ? 'Hero' : 'Monster';
+  const targetRole = target instanceof Hero ? 'Hero' : 'Monster';
+
+  if (attackerRole !== targetRole) {
+    target.life -= this.damage;
+    if (target.life <= 0) {
+      target.life = 0;
+      return addLogMsg(`${attackerRole} attacked, ${target.charClass} killed`);
     }
-    return `I am ${this.charClass} I don\`t have name`;
-  };
-
-  this.getCharClass = function getCharClass() {
-    return this.charClass;
-  };
-
-  this.attack = function attack(target) {
-    const attackerRole = this instanceof Hero ? 'Hero' : 'Monster';
-    const targetRole = target instanceof Hero ? 'Hero' : 'Monster';
-
-    if (attackerRole !== targetRole) {
-      target.life -= this.damage;
-      if (target.life <= 0) {
-        target.life = 0;
-        return addLogMsg(`${attackerRole} attacked, ${target.charClass} killed`);
-      }
-      return addLogMsg(`${attackerRole} attacked, done ${this.damage} damage to ${target.charClass}`);
-    }
-    return addLogMsg(`I will attack only ${attackerRole === 'Hero' ? 'monsters' : 'Hero'}`);
-  };
+    return addLogMsg(`${attackerRole} attacked, done ${this.damage} damage to ${target.charClass}`);
+  }
+  return addLogMsg(`I will attack only ${attackerRole === 'Hero' ? 'monsters' : 'Hero'}`);
 };
 
 const Game = function Game() {
   this.status = 'Idle';
   this.hero = undefined;
   this.monsters = [];
+};
 
-  this.beginJourney = function beginJourney() {
-    if (this.hero && this.monsters.length === 2) {
-      this.status = 'In progress';
-      return 'Your journey has started, fight monsters';
+Game.prototype.beginJourney = function beginJourney() {
+  if (this.hero && this.monsters.length === 2) {
+    this.status = 'In progress';
+    return 'Your journey has started, fight monsters';
+  }
+  return 'Cannot start journey, populate the world with hero and monsters first';
+};
+
+Game.prototype.finishJourney = function finishJourney() {
+  if (this.hero.life === 0) {
+    this.status = 'Finished';
+    return 'The Game is finished. Hero is dead :(';
+  } else if (this.monsters[0].life === 0 && this.monsters[1].life === 0) {
+    this.status = 'Finished';
+    return 'The Game is finished. Monsters are dead. Congratulations';
+  }
+  return 'Don`t stop. Some monsters are still alive. Kill`em all';
+};
+
+Game.prototype.addHero = function addHero(hero) {
+  if (!this.hero) {
+    if (hero instanceof Hero) {
+      this.hero = hero;
+      return `Hero created, welcome ${this.hero.getName()}`;
     }
-    return 'Cannot start journey, populate the world with hero and monsters first';
-  };
+    return 'Only hero instance can be hero';
+  }
+  return 'Only one hero can exist';
+};
 
-  this.finishJourney = function finishJourney() {
-    if (this.hero.life === 0) {
-      this.status = 'Finished';
-      return 'The Game is finished. Hero is dead :(';
-    } else if (this.monsters[0].life === 0 && this.monsters[1].life === 0) {
-      this.status = 'Finished';
-      return 'The Game is finished. Monsters are dead. Congratulations';
+Game.prototype.addMonster = function addMonster(monster) {
+  if (this.monsters.length < 2) {
+    if (monster instanceof Monster) {
+      this.monsters.push(monster);
+      return `Monster Created, ${monster.charClass} appeared in the world`;
     }
-    return 'Don`t stop. Some monsters are still alive. Kill`em all';
-  };
+    return 'Only monster Instances can become monsters';
+  }
+  return 'Only 2 monsters can exist';
+};
 
-  this.addHero = function addHero(hero) {
-    if (!this.hero) {
-      if (hero instanceof Hero) {
-        this.hero = hero;
-        return `Hero created, welcome ${this.hero.getName()}`;
+Game.prototype.fight = function fight() {
+  if (this.status !== 'In progress') {
+    return 'Begin your journey to start fighting monsters';
+  }
+  const currentMonster = this.monsters[0].life > 0 ? 0 : 1;
+  if (this.monsters[currentMonster].life === 0) {
+    return 'No more monsters to fight. It\'s time to finish game';
+  } else if (this.hero.life === 0) {
+    return 'Hero is died. It\'s time to finish game';
+  }
+  for (let i = 1; ; i += 1) {
+    if (i % 2 === 0) {
+      this.monsters[currentMonster].attack(this.hero);
+      if (this.hero.life === 0) {
+        return 'Monsters win';
       }
-      return 'Only hero instance can be hero';
-    }
-    return 'Only one hero can exist';
-  };
-
-  this.addMonster = function addMonster(monster) {
-    if (this.monsters.length < 2) {
-      if (monster instanceof Monster) {
-        this.monsters.push(monster);
-        return `Monster Created, ${monster.charClass} appeared in the world`;
-      }
-      return 'Only monster Instances can become monsters';
-    }
-    return 'Only 2 monsters can exist';
-  };
-
-  this.fight = function fight() {
-    if (this.status !== 'In progress') {
-      return 'Begin your journey to start fighting monsters';
-    }
-    const currentMonster = this.monsters[0].life > 0 ? 0 : 1;
-    if (this.monsters[currentMonster].life === 0) {
-      return 'No more monsters to fight. It\'s time to finish game';
-    } else if (this.hero.life === 0) {
-      return 'Hero is died. It\'s time to finish game';
-    }
-    for (let i = 1; ; i += 1) {
-      if (i % 2 === 0) {
-        this.monsters[currentMonster].attack(this.hero);
-        if (this.hero.life === 0) {
-          return 'Monsters win';
-        }
-      } else {
-        this.hero.attack(this.monsters[currentMonster]);
-        if (this.monsters[currentMonster].life === 0) {
-          return 'Hero win';
-        }
+    } else {
+      this.hero.attack(this.monsters[currentMonster]);
+      if (this.monsters[currentMonster].life === 0) {
+        return 'Hero win';
       }
     }
-  };
+  }
 };
 
 const Hero = function Hero(name, charClass) {
@@ -152,6 +144,9 @@ const Hero = function Hero(name, charClass) {
 };
 Hero.prototype = Object.create(GameCharacter.prototype);
 Hero.prototype.constructor = Hero;
+Hero.prototype.getName = function getName() {
+  return this.name;
+};
 
 const Monster = function Monster(charClass) {
   if (!charClass) {
@@ -161,6 +156,9 @@ const Monster = function Monster(charClass) {
 };
 Monster.prototype = Object.create(GameCharacter.prototype);
 Monster.prototype.constructor = Monster;
+Monster.prototype.getName = function getName() {
+  return `I am ${this.charClass} I don\`t have name`;
+};
 
 /* Game Population mechanism should go below */
 /* eslint-disable no-undef */
